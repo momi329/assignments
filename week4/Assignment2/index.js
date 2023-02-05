fa18ca25f401b2c985b51f5e1ea9c72ca17ed53b
“momi329” <amekaka@gmail.com>
2023-02-05 18:58:23 +0800

    modified assignment2

--- week4/Assignment2/index.js
+++ week4/Assignment2/index.js
@@ -1,37 +1,45 @@
 function ajax(url) {
-    // your code here
-    return fetch(url,{method: 'get'})
+  // your code here
+  return fetch(url, { method: "get" })
     .then((response) => {
-        return response.json()
+      return response.json();
     })
     .catch((error) => {
-        console.log(`Error: ${error}`);
-    })
+      console.log(`Error: ${error}`);
+    });
 }
 function render(data) {
-    // your code here
-    const datas = document.querySelector('.words');
-    const name = data.map(da => da.name);
-    const price =data.map(da => da.price);
-    const description = data.map(da => da.description);
-    datas.innerHTML=`<span class="phone"><div class="name">${name[0]}</div>
-                        <div class="price">$${price[0]}</div>
-                        <div class="description">${description[0]}</div>
-                     </span>
-                     
-                        <span class="phone"><div class="name">${name[1]}</div>
-                        <div class="price">$${price[1]}</div>
-                        <div class="description">${description[1]}</div>
-                    </span>
-                    
-                    <span class="phone"><div class="name">${name[2]}</div>
-                        <div class="price">$${price[2]}</div>
-                        <div class="description">${description[2]}</div>
-                    </span>`
-}
+  // your code here
+  const datas = document.querySelector(".words");
+  const name = data.map((da) => da.name);
+  const price = data.map((da) => da.price);
+  const description = data.map((da) => da.description);
+  let newHTML = "";
+  for (let i = 0; i < name.length; i++) {
+    newHTML += `<span class="phone"><div class="name">${name[i]}</div>
+                    <div class="price">$${price[i]}</div>
+                    <div class="description">${description[i]}</div>
+                    </span>`;
+    datas.innerHTML = newHTML;
+  }
+  //     datas.innerHTML=`<span class="phone"><div class="name">${name[0]}</div>
+  //                         <div class="price">$${price[0]}</div>
+  //                         <div class="description">${description[0]}</div>
+  //                      </span>
 
+  //                         <span class="phone"><div class="name">${name[1]}</div>
+  //                         <div class="price">$${price[1]}</div>
+  //                         <div class="description">${description[1]}</div>
+  //                     </span>
+
+  //                     <span class="phone"><div class="name">${name[2]}</div>
+  //                         <div class="price">$${price[2]}</div>
+  //                         <div class="description">${description[2]}</div>
+  //                     </span>`
+  // }
+}
 const url = "https://appworks-school.github.io/Remote-Assignment-Data/products";
 
 ajax(url).then((data) => {
-    render(data);
+  render(data);
 });
